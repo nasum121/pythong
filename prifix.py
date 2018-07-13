9327c1c9386edc22e117c65557af6186aebe9001
nasum121 <nasum.sudharshan@gmail.com>
2018-07-13 00:17:14 -0700

    Update prifix.py

--- prifix.py
+++ prifix.py
@@ -1,8 +1,58 @@
-a=int(input("\nEnter the number:"))
-b=input("\nEnter the string:")
-c=list(b)
-d=0
-while a>0:
-    print(c[d],end="")
-    d=d+1
-    a=a-1
+n=int(input())
+if n==2:
+    string1=input()
+    string2=input()
+    s1=list(string1)
+    s2=list(string2)
+    new_word=[]
+    a=len(s1)
+    b=len(s2)
+    if a>b:
+        count=b
+    else:
+        count=a
+    for i in range(count):
+        if s1[i]==s2[i]:
+            new_word.append(s1[i])
+
+    new="".join(new_word)
+    print(new)
+if n==3:
+    string1=input()
+    string2=input()
+    string3=input()
+    s1=list(string1)
+    s2=list(string2)
+    s3=list(string3)
+    new_word=[]
+    final=[]
+    a=len(s1)
+    b=len(s2)
+    c=len(s3)
+    count=min(a,b,c)
+    for i in range(count):
+        if s1[i]==s2[i]==s3[i]:
+            new_word.append(s1[i])  
+    final="".join(new_word)
+    print(final)
+if n==4:
+    string1=input()
+    string2=input()
+    string3=input()
+    string4=input()
+    s1=list(string1)
+    s2=list(string2)
+    s3=list(string3)
+    s4=list(string4)
+    new_word=[]
+    
+    a=len(s1)
+    b=len(s2)
+    c=len(s3)
+    d=len(s3)
+    count=min(a,b,c,d)
+    for i in range(count):
+        if s1[i]==s2[i]==s3[i]==s4[i]:
+            new_word.append(s[i])
+    final="".join(new_word)
+    print(final)
